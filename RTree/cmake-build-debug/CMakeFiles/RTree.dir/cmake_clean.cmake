908128ab31ada476762cc901f7c6602a3a4ec32d
Кирилл <74520222+kirilliv02@users.noreply.github.com>
2021-04-13 21:34:32 +0300

    Project

---
+++ RTree/cmake-build-debug/CMakeFiles/RTree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RTree.dir/main.cpp.obj"
+  "RTree.exe"
+  "RTree.exe.manifest"
+  "RTree.pdb"
+  "libRTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
